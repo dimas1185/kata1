b11d8a41a6381e936314c470d7c2b7d6ad7aad2a
Dmytro Sydorchenko <dmytro.sydorchenko@block.one>
2020-05-27 19:35:23 -0400

    phase 2 functionality + tests added

--- src/kata1.cpp
+++ src/kata1.cpp
@@ -1,8 +1,126 @@
 #include <eosio/eosio.hpp>
+#include <eosio/system.hpp>
 #include <eosio.token/eosio.token.hpp>
+#include <algorithm>
 
 using namespace eosio;
 
+//this is copy from eos/libraries/abieos/include/eosio/chain_conversions.hpp
+//it can't be used with eosio.cdt due to some redefinitions
+namespace
+{
+using days = std::chrono::duration
+    <int, std::ratio_multiply<std::ratio<24>, std::chrono::hours::period>>;
+
+using weeks = std::chrono::duration
+    <int, std::ratio_multiply<std::ratio<7>, days::period>>;
+
+using years = std::chrono::duration
+    <int, std::ratio_multiply<std::ratio<146097, 400>, days::period>>;
+
+using months = std::chrono::duration
+    <int, std::ratio_divide<years::period, std::ratio<12>>>;
+
+template <class Duration>
+using sys_time = std::chrono::time_point<std::chrono::system_clock, Duration>;
+
+using sys_days    = sys_time<days>;
+using sys_seconds = sys_time<std::chrono::seconds>;
+struct day {
+   inline explicit day(uint32_t d) : d(d) {}
+   uint32_t d;
+};
+struct month {
+   inline explicit month(uint32_t m) : m(m) {}
+   uint32_t m;
+};
+struct month_day {
+   inline month_day( month m, day d ) : m(m), d(d) {}
+   inline auto month() const { return m; }
+   inline auto day() const { return d; }
+   struct month m;
+   struct day d;
+};
+struct year {
+   inline explicit year( uint32_t y )
+      : y(y) {}
+   uint32_t y;
+};
+typedef year year_t;
+typedef month month_t;
+typedef day day_t;
+    struct year_month_day {
+   inline auto from_days( days ds ) {
+      const auto z = ds.count() + 719468;
+      const auto era = (z >= 0 ? z : z - 146096) / 146097;
+      const auto doe = static_cast<uint32_t>(z - era * 146097);
+      const auto yoe = (doe - doe/1460 + doe/36524 - doe/146096) / 365;
+      const auto y   = static_cast<days::rep>(yoe) + era * 400;
+      const auto doy = doe - (365 * yoe + yoe/4 - yoe/100);
+      const auto mp  = (5*doy + 2)/153;
+      const auto d   = doy - (153*mp+2)/5 + 1;
+      const auto m   = mp < 10 ? mp+3 : mp-9;
+      return year_month_day{year_t{static_cast<uint32_t>(y + (m <= 2))}, month_t(m), day_t(d)};
+   }
+   inline auto to_days() const {
+      const auto _y = static_cast<int>(y.y) - (m.m <= month_t{2}.m);
+      const auto _m = static_cast<uint32_t>(m.m);
+      const auto _d = static_cast<uint32_t>(d.d);
+      const auto era = (_y >= 0 ? _y : _y-399) / 400;
+      const auto yoe = static_cast<uint32_t>(_y - era * 400);
+      const auto doy = (153*(_m > 2 ? _m-3 : _m+9) + 2)/5 + _d-1;
+      const auto doe = yoe * 365 + yoe/4 -yoe/100 + doy;
+      return days{era * 146097 + static_cast<int>(doe) - 719468};
+   }
+   inline year_month_day(const year_t& y, const month_t& m, const day_t& d)
+      : y(y), m(m), d(d) {}
+   inline year_month_day(const year_month_day&) = default;
+   inline year_month_day(year_month_day&&) = default;
+   inline year_month_day(sys_days ds)
+      : year_month_day(from_days(ds.time_since_epoch())) {}
+   inline auto year() const { return y.y; }
+   inline auto month() const { return m.m; }
+   inline auto day() const { return d.d; }
+   year_t y;
+   month_t m;
+   day_t d;
+};
+
+inline std::string microseconds_to_str(uint64_t microseconds) {
+   std::string result;
+
+   auto append_uint = [&result](uint32_t value, int digits) {
+      char  s[20];
+      char* ch = s;
+      while (digits--) {
+         *ch++ = '0' + (value % 10);
+         value /= 10;
+      };
+      std::reverse(s, ch);
+      result.insert(result.end(), s, ch);
+   };
+
+   std::chrono::microseconds us{ microseconds };
+   sys_days                  sd(std::chrono::floor<days>(us));
+   auto                      ymd = year_month_day{ sd };
+   uint32_t                  ms  = (std::chrono::floor<std::chrono::milliseconds>(us) - sd.time_since_epoch()).count();
+   us -= sd.time_since_epoch();
+   append_uint((int)ymd.year(), 4);
+   result.push_back('-');
+   append_uint((unsigned)ymd.month(), 2);
+   result.push_back('-');
+   append_uint((unsigned)ymd.day(), 2);
+   result.push_back('T');
+   append_uint(ms / 3600000 % 60, 2);
+   result.push_back(':');
+   append_uint(ms / 60000 % 60, 2);
+   result.push_back(':');
+   append_uint(ms / 1000 % 60, 2);
+   result.push_back('.');
+   append_uint(ms % 1000, 3);
+   return result;
+}
+}
 class [[eosio::contract("kata1")]] kata1 : public eosio::contract {
 private:
     struct [[eosio::table]] acc_type_index
@@ -14,57 +132,97 @@ private:
     };
 
     using acc_table = eosio::multi_index<"acc.type.ind"_n, acc_type_index>;
+    acc_table m_types;
 
-    static name def_account()
+    struct [[eosio::table]] deffered_tx_index
     {
-        static name def ("default");
-        return def;
-    }
+        uint32_t idx;
+        name from;
+        name from_type;
+        name to;
+        name to_type;
+        asset balance;
+        uint32_t when;
+        uint32_t till;
+        bool reccuring;
+        uint32_t period;
+        uint32_t primary_key() const { return idx; }
+    };
+    using tx_table = eosio::multi_index<"tx.ind"_n, deffered_tx_index>;
+    tx_table m_txs;
+    //deffered/reoccuring transactions limit
+    static int s_trn_limit;
+    static asset sys_zero;
+    static name def_account;
 
     void updateBalance(name acc, name type, asset balance)
     {
-        acc_table types(get_self(), acc.value);
-
-        auto it = types.find(type.value);
-        if(it == types.end())
+        auto it = m_types.find(type.value);
+        if(it == m_types.end())
         {
-            print("inserting ", type, "\n");
-            check(balance > asset(0, balance.symbol), "trying to create negative balance!");
-            types.emplace(get_self(), [&](auto &row) {
-                row.type = def_account();
+            check(balance > sys_zero, "trying to create negative balance!");
+            m_types.emplace(get_self(), [&](auto &row) {
+                row.type = def_account;
                 row.balance = balance;
             });
         }
         else
         {
-            print("updating ", type, "\n");
             auto& cur_balance = it->balance;
-            check(cur_balance + balance >= asset(0, balance.symbol), "trying to create negative balance!");
-                types.modify(it, acc, [&](auto& row){
-                    row.type = type;
-                    row.balance += balance;
-                });
+            check(cur_balance + balance >= sys_zero, "trying to create negative balance!");
+            m_types.modify(it, acc, [&](auto& row){
+                row.type = type;
+                row.balance += balance;
+            });
         }
     }
 
+    void processReccuring(uint32_t idx, bool force)
+    {
+        auto it = m_txs.find(idx);
+        check(it != m_txs.end(), "processReccuring: missing reccuring transaction wih given id");
+        check(it->reccuring, "processReccuring: must be reccuring");
+
+        while (it->when <= now() || force)
+        {
+            //moving funds back and those immediately transferred. 
+            updateBalance(it->from, it->from_type, it->balance);
+            transfer(it->from, it->from_type, it->to, it->to_type, it->balance);
+            if (it->when + it->period > it->till)
+            {
+                m_txs.erase(it);
+                break;
+            }
+
+            m_txs.modify(it, get_self(), [&](auto& row){
+                row.when += row.period;
+            });
+            updateBalance(it->from, it->from_type, -it->balance);
+        }
+    }
+
+    uint32_t now() {
+      return current_time_point().sec_since_epoch();
+    }
 public:
     using contract::contract;
 
     kata1(name receiver, name code, datastream<const char *> ds) 
-    : contract(receiver, code, ds)
+    : contract(receiver, code, ds), 
+      m_types(get_self(), get_self().value),
+      m_txs(get_self(), get_self().value)
     {
-        print("kata1::contract(", get_self(), "): ", receiver, ", ", code, ", ds<>\n");
+        //print("kata1::contract(", get_self(), "): ", receiver, ", ", code, ", ds<>\n");
     }
 
     [[eosio::action]]
     void init()
     {
-        acc_table types(get_self(), get_self().value);
-        auto it = types.find(def_account().value);
-        if (it == types.end())
+        auto it = m_types.find(def_account.value);
+        if (it == m_types.end())
         {
-            types.emplace(get_self(), [&](auto &row) {
-                row.type = def_account();
+            m_types.emplace(get_self(), [&](auto &row) {
+                row.type = def_account;
                 row.balance = eosio::token::get_balance("eosio.token"_n, get_self(), symbol_code("SYS"));
             });
         }
@@ -75,11 +233,9 @@ public:
     {
         require_auth(get_self());
 
-        acc_table types(get_self(), get_self().value);
-        
-        check(types.find(type.value) == types.end(), "Account type with such name already exists");
+        check(m_types.find(type.value) == m_types.end(), "Account type with such name already exists");
 
-        types.emplace(get_self(), [&](auto &row) {
+        m_types.emplace(get_self(), [&](auto &row) {
             row.type = type;
             row.balance = asset(0, symbol("SYS", 4));
         });
@@ -89,9 +245,8 @@ public:
     void listtypes()
     {
         require_auth(get_self());
-        acc_table types(get_self(), get_self().value);
-
-        for (auto it = types.begin(); it != types.end(); ++it)
+        
+        for (auto it = m_types.begin(); it != m_types.end(); ++it)
         {
             print(it->type, "=", it->type.value, "\n");
         }
@@ -101,9 +256,8 @@ public:
     void printbal()
     {
         require_auth(get_self());
-        acc_table types(get_self(), get_self().value);
-
-        for (auto it = types.begin(); it != types.end(); ++it)
+        
+        for (auto it = m_types.begin(); it != m_types.end(); ++it)
         {
             print(it->type, ":\n");
             print(it->balance.to_string(), "\n");
@@ -113,22 +267,31 @@ public:
     [[eosio::on_notify("eosio.token::transfer")]]
     void deposit(name from, name to, eosio::asset balance, std::string memo) 
     {
-        print("deposit called(", get_self(), "): ", from, ", ", to, ", balance=", balance, ", ", memo, "\n");
+        print("deposit called(", get_self(), "): ", from, ", ", to, ", balance=", balance, ", memo=", memo, "\n");
         if (to != get_self() && from != get_self())
             return;
 
         check(balance.amount > 0, "zero quantity, aborting");
 
-        if (to == get_self())
-            updateBalance(to, def_account(), balance);
+        name account_type;
+        if (memo.empty())
+            account_type = def_account;
         else
-            updateBalance(from, def_account(), -balance);
+            account_type = m_types.get(name(memo).value).type;
+        
+        if (to == get_self())
+            updateBalance(to, account_type, balance);
+        else if (from == get_self())
+            updateBalance(from, account_type, -balance);
     }
 
     [[eosio::action]]
     void transfer(name from, name from_type, name to, name to_type, asset balance)
     {
         require_auth(from);
+
+        check( balance.amount > 0, "attepmt to transfer negtive quantity, calling crypto cops..." );
+        print("transfer: ", from, "[", from_type, "] -> ", to, "[", to_type, "] ", balance.amount, "\n");
         //transfer between owner accounts
         if (from == to)
         {
@@ -143,7 +306,7 @@ public:
                 permission_level{get_self(), "active"_n},
                 "eosio.token"_n,
                 "transfer"_n,
-                std::make_tuple(get_self(), to, balance, std::string("kata1 transfer"))
+                std::make_tuple(get_self(), to, balance, from_type.to_string())
             }.send();
         }
     }
@@ -153,10 +316,116 @@ public:
     {
         require_auth(get_self());
 
-        acc_table types(get_self(), get_self().value);
-        
-        auto it_c = types.find(type.value);
-        check(it_c != types.end(), "account type not found");
+        auto it_c = m_types.find(type.value);
+        check(it_c != m_types.end(), "account type not found");
+        print("verifying balance: ", it_c->balance.amount, " == ", balance.amount);
         check(it_c->balance.amount == balance.amount, "wrong balance!");
     }
+
+    [[eosio::action]]
+    void executedef(bool force)
+    {
+        require_auth(get_self());
+        
+        auto it = m_txs.begin();
+        check(it != m_txs.end(), "empty deffered transactions list");
+
+        while (it != m_txs.end())
+        {
+            if (force || it->when <= now())
+            {
+                if (it->reccuring)
+                {
+                    uint32_t idx = it->idx;
+                    it++;
+                    processReccuring(idx, force);
+                }
+                else
+                {
+                    updateBalance(it->from, it->from_type, it->balance);
+                    transfer(it->from, it->from_type, it->to, it->to_type, it->balance);
+                    it = m_txs.erase(it);
+                }
+            }
+            else
+            {
+                it++;
+            }
+        }
+    }
+
+    [[eosio::action]]
+    void deffered(int idx,
+                  name from, 
+                  name from_type, 
+                  name to, 
+                  name to_type, 
+                  asset balance, 
+                  uint32_t when, 
+                  bool reccuring, 
+                  uint32_t till, 
+                  uint32_t period)
+    {
+        require_auth(from);
+        check( balance.amount > 0, "deffered: attepmt to transfer negtive quantity, calling crypto cops..." );
+        check( when >= now(), "deffered transaction can't be in past");
+        size_t count = std::count_if(m_txs.begin(), m_txs.end(), [](auto& x){return true;});
+        check( count < s_trn_limit, "transactions buffer is full");
+        
+        updateBalance(from, from_type, -balance);
+
+        check(m_txs.find(idx) == m_txs.end(), "transaction with such id already exists");
+
+        m_txs.emplace(from, [&](auto& row){
+            row.idx = idx;
+            row.from = from;
+            row.from_type = from_type;
+            row.to = to;
+            row.to_type = to_type;
+            row.balance = balance;
+            row.when = when;
+            row.till = till;
+            row.reccuring = reccuring;
+            row.period = period;
+        });
+    }
+
+    [[eosio::action]]
+    void canceltrn(int idx)
+    {
+        require_auth(get_self());
+
+        auto it = m_txs.find(idx);
+        check(it != m_txs.end(), "no transaction with such id");
+        updateBalance(it->from, it->from_type, it->balance);
+
+        m_txs.erase(it);
+    }
+
+    [[eosio::action]]
+    void verifydef(size_t number)
+    {
+        size_t count = std::count_if(m_txs.begin(), m_txs.end(), [](auto& x){return true;});
+        check(number == count, "deffered transactions count doesn't match");
+    }
+
+    [[eosio::action]]
+    void printdef()
+    {
+        print("deffered transfers:\n");
+        for (auto it : m_txs)
+        {
+            print(it.from, "[", it.from_type, "] -> ", 
+                  it.to, "[", it.to_type, "] ", 
+                  it.balance.amount, " at ", 
+                  microseconds_to_str(it.when * 1000000ull),
+                  " reccuring=", (it.reccuring ? "true" : "false"),
+                  "\n");
+        }
+        print("-------------------\n");
+    }
 };
+
+int kata1::s_trn_limit = 10;
+asset kata1::sys_zero = asset(0, symbol("SYS", 4));
+name kata1::def_account("default");
--- tests/kata1_test.cpp
+++ tests/kata1_test.cpp
@@ -8,7 +8,6 @@
 using namespace eosio;
 using namespace eosio::chain;
 using namespace eosio::testing;
-using namespace fc;
 
 BOOST_AUTO_TEST_SUITE(kata1_tests)
 
@@ -25,10 +24,6 @@ BOOST_AUTO_TEST_CASE(post) try {
     t.set_abi(N(kata1), eosio::testing::contracts::kata1_abi().data());
     t.produce_block();
 
-    // Create users
-    //t.create_account(N(eosio.token));
-    //t.create_account(N(bob));
-
     // create SYS token
     t.push_action(
         N(eosio.token), N(create), N(eosio.token),
@@ -43,7 +38,7 @@ BOOST_AUTO_TEST_CASE(post) try {
         mutable_variant_object
         ("to", "eosio.token")
         ("quantity", "1000.0000 SYS")
-        ("memo", "memo")
+        ("memo", "")
     );
 
     //transfer some to kata1
@@ -53,7 +48,7 @@ BOOST_AUTO_TEST_CASE(post) try {
         ("from", "eosio.token")
         ("to", "kata1")
         ("quantity", "100.0000 SYS")
-        ("memo", "memo")
+        ("memo", "")
     );
 
     t.push_action(
@@ -163,6 +158,23 @@ BOOST_AUTO_TEST_CASE(post) try {
         ("balance", "85.0000 SYS")
     );
 
+    //transfer some to kata1
+    t.push_action(
+        N(eosio.token), N(transfer), N(eosio.token),
+        mutable_variant_object
+        ("from", "eosio.token")
+        ("to", "kata1")
+        ("quantity", "1.0000 SYS")
+        ("memo", "checking")
+    );
+
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "checking")
+        ("balance", "6.0000 SYS")
+    );
+
     //try to overdraft
     BOOST_CHECK_THROW(
         [&] {
@@ -177,6 +189,315 @@ BOOST_AUTO_TEST_CASE(post) try {
             );
         }(),
     fc::exception);
+
+    uint32_t sec_since_epoch = t.control->pending_block_time().time_since_epoch().count() / 1000000;
+
+    //deffered transfer between types
+    t.push_action(
+        N(kata1), N(deffered), N(kata1),
+        mutable_variant_object
+        ("idx", 1)
+        ("from", "kata1")
+        ("from_type", "default")
+        ("to", "eosio.token")
+        ("to_type", "")
+        ("balance", "10.0000 SYS")
+        ("when", sec_since_epoch)
+        ("till", 0)
+        ("reccuring", false)
+        ("period", 0)
+    );
+
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 1)
+    );
+
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "75.0000 SYS")
+    );
+
+    t.push_action(
+        N(kata1), N(executedef), N(kata1),
+        mutable_variant_object
+        ("force", false)
+    );
+
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 0)
+    );
+
+    //deferred in past should fail
+    BOOST_CHECK_THROW(
+        [&] {
+            t.push_action(
+                N(kata1), N(deffered), N(kata1),
+                mutable_variant_object
+                ("idx", 1)
+                ("from", "kata1")
+                ("from_type", "default")
+                ("to", "eosio.token")
+                ("to_type", "")
+                ("balance", "10.0000 SYS")
+                ("when", sec_since_epoch - 1)
+                ("till", 0)
+                ("reccuring", false)
+                ("period", 0)
+            );
+        }(),
+    fc::exception);
+
+    t.produce_blocks();
+    sec_since_epoch = t.control->pending_block_time().time_since_epoch().count() / 1000000;
+
+        //transfer between types
+    t.push_action(
+        N(kata1), N(deffered), N(kata1),
+        mutable_variant_object
+        ("idx", 1)
+        ("from", "kata1")
+        ("from_type", "default")
+        ("to", "eosio.token")
+        ("to_type", "")
+        ("balance", "1.0000 SYS")
+        ("when", sec_since_epoch)
+        ("till", sec_since_epoch + 90)
+        ("reccuring", true)
+        ("period", 10)
+    );
+    t.produce_blocks();
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "74.0000 SYS")
+    );
+
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 1)
+    );
+    t.produce_blocks();
+
+    t.push_action(
+        N(kata1), N(executedef), N(kata1),
+        mutable_variant_object
+        ("force", false)
+    );
+    t.produce_blocks();
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "73.0000 SYS")
+    );
+    t.produce_blocks();
+
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 1)
+    );
+    t.produce_blocks();
+
+    t.push_action(
+        N(kata1), N(executedef), N(kata1),
+        mutable_variant_object
+        ("force", true)
+    );
+    t.produce_blocks();
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "65.0000 SYS")
+    );
+    t.produce_blocks();
+
+    sec_since_epoch = t.control->pending_block_time().time_since_epoch().count() / 1000000;
+    //try to deposit to invalid account
+    t.push_action(
+        N(kata1), N(deffered), N(kata1),
+        mutable_variant_object
+        ("idx", 1)
+        ("from", "kata1")
+        ("from_type", "default")
+        ("to", "invalid")
+        ("to_type", "")
+        ("balance", "10.0000 SYS")
+        ("when", sec_since_epoch + 1)
+        ("till", 0)
+        ("reccuring", false)
+        ("period", 0)
+    );
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 1)
+    );
+    t.produce_blocks();
+    BOOST_CHECK_THROW(
+        [&] {
+            t.push_action(
+                N(kata1), N(executedef), N(kata1),
+                mutable_variant_object
+                ("force", true)
+            );
+        }(),
+    fc::exception);
+
+    t.push_action(
+        N(kata1), N(canceltrn), N(kata1),
+        mutable_variant_object
+        ("idx", 1)
+    );
+    t.produce_blocks();
+
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 0)
+    );
+
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "65.0000 SYS")
+    );
+    t.produce_blocks();
+
+    sec_since_epoch = t.control->pending_block_time().time_since_epoch().count() / 1000000;
+    for (uint32_t i = 0; i < 10; i++ )
+    {
+        //deffered transfer between types
+        t.push_action(
+            N(kata1), N(deffered), N(kata1),
+            mutable_variant_object
+            ("idx", i)
+            ("from", "kata1")
+            ("from_type", "default")
+            ("to", "eosio.token")
+            ("to_type", "")
+            ("balance", "1.0000 SYS")
+            ("when", sec_since_epoch + i)
+            ("till", 0)
+            ("reccuring", false)
+            ("period", 0)
+        );
+    }
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 10)
+    );
+    t.push_action(
+        N(kata1), N(printdef), N(kata1),
+        mutable_variant_object()
+    );
+    t.produce_blocks();
+
+    BOOST_CHECK_THROW(
+        [&] {
+            t.push_action(
+                N(kata1), N(deffered), N(kata1),
+                mutable_variant_object
+                ("idx", 11)
+                ("from", "kata1")
+                ("from_type", "default")
+                ("to", "eosio.token")
+                ("to_type", "")
+                ("balance", "10.0000 SYS")
+                ("when", sec_since_epoch + 10)
+                ("till", 0)
+                ("reccuring", false)
+                ("period", 0)
+            );
+        }(),
+    fc::exception);
+    t.produce_blocks();
+
+    t.push_action(
+        N(kata1), N(executedef), N(kata1),
+        mutable_variant_object
+        ("force", false)
+    );
+    //3 blocks passed so 10-2 is 8
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 8)
+    );
+    t.produce_blocks();
+    t.push_action(
+        N(kata1), N(executedef), N(kata1),
+        mutable_variant_object
+        ("force", true)
+    );
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 0)
+    );
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "55.0000 SYS")
+    );
+    t.produce_blocks();
+
+    sec_since_epoch = t.control->pending_block_time().time_since_epoch().count() / 1000000;
+    //deffered transfer between types
+    t.push_action(
+        N(kata1), N(deffered), N(kata1),
+        mutable_variant_object
+        ("idx", 1)
+        ("from", "kata1")
+        ("from_type", "default")
+        ("to", "kata1")
+        ("to_type", "checking")
+        ("balance", "1.0000 SYS")
+        ("when", sec_since_epoch)
+        ("till", 0)
+        ("reccuring", false)
+        ("period", 0)
+    );
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 1)
+    );
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "default")
+        ("balance", "54.0000 SYS")
+    );
+    t.produce_blocks();
+    t.push_action(
+        N(kata1), N(executedef), N(kata1),
+        mutable_variant_object
+        ("force", false)
+    );
+    t.push_action(
+        N(kata1), N(verifydef), N(kata1),
+        mutable_variant_object
+        ("number", 0)
+    );
+    t.push_action(
+        N(kata1), N(verify), N(kata1),
+        mutable_variant_object
+        ("type", "checking")
+        ("balance", "7.0000 SYS")
+    );
 }
 FC_LOG_AND_RETHROW()
 
